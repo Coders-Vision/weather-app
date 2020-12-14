cd83144db407dab5f0cc07da7de7bd29d0a7f69f
unknown <fowzan.alpha@gmail.com>
2020-12-15 00:14:37 +0300

    Fixed Misc.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Forecast from './components/Forecast'
 import AutoSuggest from './components/AutoSuggestField'
 import cities from './weather-cities.json'
 
-console.log(process.env.REACT_APP_API_KEY)
 const weatherById = placeid => `https://api.openweathermap.org/data/2.5/forecast?id=${placeid}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
 const weatherByGPS = gpsData => `https://api.openweathermap.org/data/2.5/forecast?lat=${gpsData.lat}&lon=${gpsData.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
 
